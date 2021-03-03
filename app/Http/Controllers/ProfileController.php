45226eada93f021fbb8fd740f81fc71b9a2ea42a
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2021-03-03 15:38:31 -0300

    recibiendo y manejando archivos en laravel, utilizando la imagen de perfil del usuario

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\ProfileRequest;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -21,19 +23,32 @@ class ProfileController extends Controller
 
     public function update(ProfileRequest $request)
     {
-        $user = $request->user();
-
-        $user->fill( $request->validated() );
-
-        if ( $user->isDirty('email') ) {
-            $user->email_verified_at = null;
-            $user->sendEmailVerificationNotification();
-        }
-
-        $user->save();
-
-        return redirect()
-            ->route('profile.edit')
-            ->withSuccess('Profile edited');
+        return DB::transaction(function () use ($request) {
+            $user = $request->user();
+    
+            $user->fill( $request->validated() );
+    
+            if ( $user->isDirty('email') ) {
+                $user->email_verified_at = null;
+                $user->sendEmailVerificationNotification();
+            }
+    
+            $user->save();
+    
+            if ( $request->hasFile('image') ) {
+                if ( $user->image != null ) {
+                    Storage::disk('images')->delete($user->image->path);
+                    $user->image->delete();
+                }
+    
+                $user->image()->create([
+                    'path' => $request->image->store('users', 'images'),
+                ]);
+            }
+            
+            return redirect()
+                ->route('profile.edit')
+                ->withSuccess('Profile edited');
+        }, 5);
     }
 }
