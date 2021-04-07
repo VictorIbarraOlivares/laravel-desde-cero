221d54f078e24a61abfe60caf30fedb18566600d
unknown <victor.ibarra.olivares@gmail.com>
2021-04-07 09:48:15 -0400

    se utiliza soft delete en los productos

--- app/Product.php
+++ app/Product.php
@@ -5,9 +5,11 @@ use App\Order;
 use App\Image;
 use App\Scopes\AvailableScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
     protected $table = 'products';
     
     /**
